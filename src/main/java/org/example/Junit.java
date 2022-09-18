750d3c43fd617139be1fd5ace2f58c57dca8a0c2
Dinesh Reddy <rdinesh640@gmail.com>
2022-09-18 13:51:27 +0100

    written new test cases

--- src/main/java/org/example/Junit.java
+++ src/main/java/org/example/Junit.java
@@ -2,42 +2,76 @@ package org.example;
 
 public class Junit {
 
-    public int add(int a, int b){
-        return a+b;
+    private Object NullPointerException;
+
+    public int add(int a, int b) {
+        return a + b;
     }
 
-    public int sub(int a, int b){
-        return a-b;
+    public int sub(int a, int b) {
+        return a - b;
     }
-    public Boolean addIfElse(int a, int b){
-        if(a>1){
+
+    public Boolean addIfElse(int a, int b) {
+        if (a > 1) {
             return true;
-        }
-        else{
+        } else {
             return false;
         }
     }
-    public int subIfElse(int a, int b){
-        if(a>1){
-            return a-b;
-        }
-        else{
-            return a-b+a;
+
+    public int subIfElse(int a, int b) {
+        if (a > 1) {
+            return a - b;
+        } else {
+            return a - b + a;
         }
     }
 
-    public Boolean nestedIfCondition(int a, int b, int c){
-        if(a>b){
-            if(a>c){
+    public Boolean nestedIfCondition(int a, int b, int c) {
+        if (a > b) {
+            if (a > c) {
                 return true;
-            }
-            else{
+            } else {
                 return false;
             }
         }
         return false;
     }
-    public int mul(int a, int b){
-        return a*b;
+
+    public int mul(int a, int b) {
+        return a * b;
+    }
+
+
+    public String nameOfPerson(String name, String id){
+        if(name == "Dinesh"){
+            return  id; //1
+        }
+        if(name == "Lisa"){
+            return id;//2
+        }
+        if(name == "Peter"){
+            return id;//3
+        }
+        return "no name matched";
+    }
+
+    public void findUser(String name) throws Exception { // throws Exception will be used when we thow excpetion to the method.
+        try{
+            System.out.println(name.toString());// prints the name of the person
+        }
+        catch(NullPointerException e){
+            throw new NullPointerException("Exception message");
+        }
+    }
+
+    public String checkException(String name) throws Exception { // throws Exception will be used when we thow excpetion to the method.
+        try{
+            return name.toString();
+        }
+        catch(NullPointerException e){
+            throw new NullPointerException("Exception message");
+        }
     }
 }
--- src/test/java/org/example/JunitTest.java
+++ src/test/java/org/example/JunitTest.java
@@ -70,4 +70,51 @@ public class JunitTest {
         Assert.assertEquals(10, c);
     }
 
+    @Test
+    public void testfindIdOfPerson_dinesh(){
+        Junit junit = new Junit(); // to call the method in the class Junit, just to make the connection form JunitTest class to junit class.
+        String id = junit.nameOfPerson("Dinesh", "1");// calls the method with parameters.
+        Assert.assertEquals("1",id);// checking the return value from the method same as expected
+    }
+    @Test
+    public void testfindIdOfPerson_lisa(){
+        Junit junit = new Junit(); // to call the method in the class Junit, just to make the connection form JunitTest class to junit class.
+        String id = junit.nameOfPerson("Lisa", "2");// calls the method with parameters.
+        Assert.assertEquals("2",id);// checking the return value from the method same as expected
+    }
+    @Test
+    public void testfindIdOfPerson_peter(){
+        Junit junit = new Junit(); // to call the method in the class Junit, just to make the connection form JunitTest class to junit class.
+        String id = junit.nameOfPerson("Peter", "3");// calls the method with parameters.
+        Assert.assertEquals("3",id);// checking the return value from the method same as expected
+    }
+    @Test
+    public void testfindIdOfPerson_NoNameMatched() {
+        Junit junit = new Junit(); //  This line is to create new object for Junit.
+        String id = junit.nameOfPerson("Kim","4"); // junit is the reference of Junit object.//nameOfPerson() is the method in Junit class.
+        Assert.assertEquals("no name matched", id);
+    }
+
+    @Test
+    public void testFindUser() throws Exception {
+        Junit junit = new Junit();
+        junit.findUser("Dinesh");
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testFinduser_NullPointerException() throws Exception {
+        Junit junit = new Junit();
+        junit.findUser(null);
+    }
+
+    @Test
+    public void testCheckException() throws Exception {
+        Junit junit = new Junit();
+        junit.checkException("Dinesh");
+    }
+    @Test(expected = NullPointerException.class)
+    public void testCheckException_NullPointerException() throws Exception {
+        Junit junit = new Junit();
+        junit.checkException(null);
+    }
 }
