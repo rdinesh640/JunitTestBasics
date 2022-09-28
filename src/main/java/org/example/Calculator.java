d8e9782e7a9d19d8046e160194b56f92a4671223
Dinesh Reddy <rdinesh640@gmail.com>
2022-09-28 20:14:52 +0100

    added new class

---
+++ src/main/java/org/example/Calculator.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class Calculator {
+
+    public int add(int a,int b, int c){
+        return a+b+c;
+    }
+
+    public int sub(int a,int b, int c){
+        return a-b-c;
+    }
+
+    public int mul(int a,int b, int c){
+        try {
+            return a * b * c;
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+        return a;
+    }
+
+}
