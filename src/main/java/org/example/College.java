808d5b312a0801cd8f7ddbea1e785d46917fed83
lisas <lisa.spencer1111@outlook.com>
2022-09-29 15:40:05 -0400

    Wrote test cases for College.java

--- src/main/java/org/example/College.java
+++ src/main/java/org/example/College.java
@@ -2,13 +2,13 @@ package org.example;
 
 public class College {
 
-    private Student student;
-    private Teacher teacher;
-    private int id = 145673;
+    private Student student;  // saving the object in the student variable; this is the same as declaring the variable student
+    private Teacher teacher;  // saving the object in the teacher variable; this is the same as declaring the variable teacher
+    private int id = 145673;  // assigning a value to the variable id  which is of type int. The value being assigned is 145673.
 
-    public College(Student student, Teacher teacher) {
-        this.student = student;
-        this.teacher = teacher;
+    public College(Student student, Teacher teacher) {   // constructor method does not creat new objects; but getting/carrying the object references to the constructor
+        this.student = student;  // this.student is the same as on Line 5
+        this.teacher = teacher;  // this.teacher is the same as on Line 6
     }
 
     public String findCollege(){
---
+++ src/test/java/org/example/CollegeTest.java
@@ -0,0 +1,52 @@
+package org.example;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+public class CollegeTest {
+
+    Teacher teacher = Mockito.mock(Teacher.class);  // Creating a new mock object of Teacher class. *Note: Mockito is a class; and mock is a method; containing parameter Teacher.class. Works with the when-then statements below
+    Student student = Mockito.mock(Student.class);  // Creating a new mock object of Student class. *Note: Mockito is a class; and mock is a method; containing parameter Student.class. Works with the when-then statements below
+
+    College college;
+
+    @Before  // This Before method will be run before each @Test is run
+    public void setUp() {
+        college = new College(student, teacher);  // created object for College class
+
+    }
+
+    @Test
+    public void testFindStudentAndTeachers() {
+        String res = college.findStudentAndTeachers(12345, "Abby");  // calling findStudentAndTeachers() method from College class. college is the object reference for College class.
+                                                                                  // When I ctrl + click findStudentAndTeachers, then I can see the parameters to enter in the ()
+        Assert.assertEquals("no student found", res);
+
+    }
+
+    @Test
+    public void testFindStudentAndTeachers2() {
+        when(student.findStudentAndTeacher(123)).thenReturn("Dinesh");  // Works with the mock statements above Line 14
+        when(teacher.findTeacher1()).thenReturn("Lisa");  // Works with the mock statements above Line 13
+        String res = college.findStudentAndTeachers(145673, "Abby");  // calling findStudentAndTeachers() method from College class. college is the object reference for College class.
+        Assert.assertNotNull(res);
+
+    }
+
+    @Test
+    public void testFindStudentAndTeachers3() {
+        String res = college.findStudentAndTeachers(12345, null);
+        Assert.assertEquals("name is null", res);
+    }
+
+    @Test
+    public void testFindStudentAndTeachers4() {
+        String res = college.findCollege();
+        Assert.assertEquals("Griffith", res);
+    }
+}
