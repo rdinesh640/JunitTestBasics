81e26cdc93927ca6551090cdeaee74eaa8464d43
Dinesh Reddy <rdinesh640@gmail.com>
2022-09-29 18:13:18 +0100

    new method added

--- src/main/java/org/example/College.java
+++ src/main/java/org/example/College.java
@@ -2,8 +2,30 @@ package org.example;
 
 public class College {
 
+    private Student student;
+    private Teacher teacher;
+    private int id = 145673;
+
+    public College(Student student, Teacher teacher) {
+        this.student = student;
+        this.teacher = teacher;
+    }
+
     public String findCollege(){
         return "Griffith";   // "Griffith" is the response (res) we will receive when we call the findCollege() method in Student.java
                              // It is the college.findCollege(); method that we are calling from Student.java
+    }
+    public String findStudentAndTeachers(int personId, String name){
+        if (name !=null){
+            if(personId ==id){
+                student.findStudentAndTeacher(id);
+                teacher.findTeacher1();
+                return "student found";
+            }
+            else{
+                return "no student found";
+            }
+        }
+        return "name is null";
     }
 }
