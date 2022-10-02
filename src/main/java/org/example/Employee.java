f649cdccd2f07467477b5c98a8229559ddd93a81
Dinesh Reddy <rdinesh640@gmail.com>
2022-10-02 12:34:22 +0100

    added small change

--- src/main/java/org/example/Employee.java
+++ src/main/java/org/example/Employee.java
@@ -3,22 +3,26 @@ package org.example;
 import org.springframework.util.StringUtils;
 
 public class Employee {
-  public String findEmployee(String employeeId, String callerId){
-      if(employeeId.length() > 8){
-          return "IT Department";
-      }
-      else if (employeeId.length() < 8) {
+  public String findEmployee(String employeeId, String callerId) {
+      try {
+          if (employeeId.length() > 8) {
+              return "IT Department";
+          } else if (employeeId.length() < 8) {
               return "Software Department";
           }
-      if(callerId.length()<10){
-          return "Azure Deparment";
-      }
-      else if (callerId.length() > 10) {
-              return "Aws Deparment";
+      } catch (Exception e) {
+          try {
+              if (callerId.length() < 10) {
+                  return "Azure Deparment";
+              } else if (callerId.length() > 10) {
+                  return "Aws Deparment";
+              }
           }
-      else{
-          return "no Deparment Found";
+          catch (Exception e1){
+              System.out.println(e1);
+          }
       }
+      return "no Deparment Found";
   }
 
 }
