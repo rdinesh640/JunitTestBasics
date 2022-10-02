1c42aa158879e8968b0c4094b022bd673a0a3e54
lisas <lisa.spencer1111@outlook.com>
2022-10-02 08:11:23 -0400

    Wrote test cases for Employee.java

--- src/main/java/org/example/Employee.java
+++ src/main/java/org/example/Employee.java
@@ -3,26 +3,26 @@ package org.example;
 import org.springframework.util.StringUtils;
 
 public class Employee {
-  public String findEmployee(String employeeId, String callerId) {
+  public String findEmployee(String employeeId, String callerId) {   // In the test case we have made the value of employeeId = null; so here on lines 6 & 8 employeeId = null
       try {
           if (employeeId.length() > 8) {
               return "IT Department";
           } else if (employeeId.length() < 8) {
               return "Software Department";
           }
-      } catch (Exception e) {
+      } catch (Exception e) {   // We can write a test case for catch block by making employeeId = null
           try {
-              if (callerId.length() < 10) {
-                  return "Azure Deparment";
+              if (callerId.length() < 10) {   // As a result of our making employeeId = null in the test, here in code line 8 checks condition. Since employeeId = null,
+                  return "Azure Department";   // the program will skip lines 9-12 and go to the catch block line 13. Expected result is the return "Azure Department"
               } else if (callerId.length() > 10) {
-                  return "Aws Deparment";
+                  return "Aws Department";
               }
           }
           catch (Exception e1){
               System.out.println(e1);
           }
       }
-      return "no Deparment Found";
+      return "no Department Found";
   }
 
 }
--- src/test/java/org/example/EmployeeTest.java
+++ src/test/java/org/example/EmployeeTest.java
@@ -16,15 +16,15 @@ public class EmployeeTest {
     @Before
     public void setUp() {
         employee = new Employee();  // We are initializing the Employee object. That is the same as creating a new object of Employee class
-                                    // This Before method will be run before each @Test is run
-                                    // to avoid creating new object of class Employee within each @Test
-                                    // But by using @Before, we are still creating for each @Test
+        // This Before method will be run before each @Test is run
+        // to avoid creating new object of class Employee within each @Test
+        // But by using @Before, we are still creating for each @Test
     }
 
     @Test
     public void testFindEmployee() {
         //Employee employee = new Employee(); // create new object of class Employee; where employee is the reference to the Employee class
-                                             // Since we have @Before we do not need to write Line 26 Employee employee = new Employee();
+        // Since we have @Before we do not need to write Line 26 Employee employee = new Employee();
         String res = employee.findEmployee("1234567", "9876");
         Assert.assertEquals("Software Department", res);
     }
@@ -32,12 +32,26 @@ public class EmployeeTest {
     @Test
     public void testFindEmployee2() {
         String res = employee.findEmployee("123456789", "9876");  // Insert the parameters
-                                        // be sure to make the employeeId to satisfy the requirement in the if condition if(employeeId.length() > 8)
+        // be sure to make the employeeId to satisfy the requirement in the if condition if(employeeId.length() > 8)
         Assert.assertEquals("IT Department", res);
     }
 
+    @Test
+    public void testFindEmployeeNull() {
+        String res = employee.findEmployee(null, "9876");  // Insert the parameters
+        Assert.assertEquals("Azure Department", res);
 
+    }
 
+    @Test
+    public void testFindCallerIdGreaterThanTen() {
+        String res = employee.findEmployee(null, "98765432109");  // Insert the parameters
+        Assert.assertEquals("Aws Department", res);
+    }
 
-
+    @Test
+    public void testFindCallerIdNull() {
+        String res = employee.findEmployee(null, null);  // Insert the parameters
+        Assert.assertEquals("no Department Found", res);
+    }
 }
