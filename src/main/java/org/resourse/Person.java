cb8d608d0bf66e40b0b5ab2e2acb2b2e7cafc509
Dinesh Reddy <rdinesh640@gmail.com>
2022-10-08 13:32:41 +0100

    added small change

---
+++ src/main/java/org/resourse/Person.java
@@ -0,0 +1,13 @@
+package org.resourse;
+
+public class Person {
+
+    public String findPersonId(String name){
+        if (name == "lisa"){
+            return "9832";
+        }
+        else{
+            return "6732";
+        }
+    }
+}
