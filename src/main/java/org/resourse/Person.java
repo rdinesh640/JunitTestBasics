3f1b1655da3fce9eaac33e85fd9c222cb3510f49
Dinesh Reddy <rdinesh640@gmail.com>
2022-10-08 14:06:33 +0100

    added small change

--- src/main/java/org/resourse/Person.java
+++ src/main/java/org/resourse/Person.java
@@ -10,4 +10,19 @@ public class Person {
             return "6732";
         }
     }
+
+    public String findPersonIds(String name){
+        try {
+            if (name == "lisa") {
+                name.length();
+                return "9832";
+            }
+            else{
+                return "6732";
+            }
+        }
+        catch (NullPointerException e){
+            return "6762";
+        }
+    }
 }
